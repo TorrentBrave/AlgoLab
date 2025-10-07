4a8ab6be5282d666d78610bcc191d8caabd4bb5d
BoHao Chen <chenbohao@stu.xupt.edu.cn>
2025-10-08 00:41:21 +0800

    doc: basic_改变张量形状

--- dd.py
+++ dd.py
@@ -1,13 +1,20 @@
 import torch
 
 def main():
-    ones_tensor  = torch.ones([2, 3, 4, 5])
-    print(ones_tensor.ndim)
-    print(ones_tensor.shape)
-    print(ones_tensor.shape[-1])
-    print(ones_tensor.size())
-    print(ones_tensor.numel())
-    print(ones_tensor)
+    ndim_3_Tensor = torch.tensor([[[1, 2, 3, 4, 5],
+                                 [6, 7, 8, 9, 10]],
+
+                                 [[11, 12, 13, 14, 15],
+                                 [16, 17, 18, 19, 20]],
+                                 
+                                 [[21, 22, 23, 24, 25],
+                                 [26, 27, 28, 29, 30]]])
     
+    print(ndim_3_Tensor.shape)
+    print(ndim_3_Tensor)
+
+    reshape_Tensor = torch.reshape(ndim_3_Tensor, [2, 5, 3])
+    print(reshape_Tensor)
+
 if __name__ == "__main__":
     main()
