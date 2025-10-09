2a69ac714dff2e7fea2991bc9be714f2c1bb09ee
BoHao Chen <chenbohao@stu.xupt.edu.cn>
2025-10-09 14:45:08 +0800

    doc: basic_张量设备位置

--- basics/dd_4.py
+++ basics/dd_4.py
@@ -2,7 +2,4 @@ import torch
 ndim2_Tensor = torch.ones([5, 10])
 
 print(ndim2_Tensor.dtype)
-new_Tensor= ndim2_Tensor.to(torch.int64)
-print(new_Tensor.dtype)
-print(new_Tensor)
-print()
+print(ndim2_Tensor.device)
